0af8c5480a2caa8b37d7d1aa3040f6b272eb172a
Reyser Zapata <reyserlyn@gmail.com>
2025-05-30 15:16:55 -0500

    fix problem gitignore

--- drizzle/migrations/0001_tricky_thunderbolts.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "order_items" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "orders" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "products" ENABLE ROW LEVEL SECURITY;
