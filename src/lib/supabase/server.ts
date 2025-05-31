b18d491a9302a81ad467db4c81b491ac3f56f7c8
Reyser Zapata <reyserlyn@gmail.com>
2025-05-31 03:24:56 -0500

    linter and formater

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,13 +2,13 @@ import { auth } from '@clerk/nextjs/server'
 import { createClient } from '@supabase/supabase-js'
 
 export function createServerSupabaseClient() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      async accessToken() {
-        return (await auth()).getToken()
-      },
-    },
-  )
+	return createClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			async accessToken() {
+				return (await auth()).getToken()
+			},
+		},
+	)
 }
